08b045af5e0f810dec9b4e6dcc5919ad26908f4e
Rahul Pereira <rahulpereira5@gmail.com>
2023-05-14 21:08:04 +1000

    Adding functionality and requirements

--- appv2.py
+++ appv2.py
@@ -5,6 +5,9 @@ from tkinter import messagebox
 
 from sql_connector import SQLiteConnector
 
+# A global variable to store and retrieve logged in users.
+USER_ID = None
+
 
 class RegisterPage:
     def __init__(self, master):
@@ -29,14 +32,14 @@ class RegisterPage:
         self.password_re_entry = tk.Entry(self.master, show="*")
         self.password_re_entry.grid(row=2, column=1)
 
-        self.login_button = tk.Button(
+        self.register_button = tk.Button(
             self.master,
             text="Register",
             command=self.register,
             highlightcolor="#38726c",
             highlightbackground="#38726c",
         )
-        self.login_button.grid(row=3, column=1)
+        self.register_button.grid(row=3, column=1)
 
         self.login_page = tk.Button(
             self.master,
@@ -47,6 +50,7 @@ class RegisterPage:
         )
         self.login_page.grid(row=4, column=1)
 
+        # Instance of a database
         self._db_connector = SQLiteConnector(database="database/shop.db")
         self.create_table()
 
@@ -61,6 +65,7 @@ class RegisterPage:
         self._db_connector.execute_insert_query(create_table_query)
 
     def get_login_page(self):
+        # Redirects to login page
         self.master.withdraw()
         login_root = tk.Tk()
         login_root.configure(background="#38726c")
@@ -78,7 +83,7 @@ class RegisterPage:
             messagebox.showerror("Error", "Password doesn't match")
             return
 
-        result = self._db_connector.execute_insert_query(
+        self._db_connector.execute_insert_query(
             "INSERT INTO USERS(username, password) VALUES(?, ?)", (username, password)
         )
 
@@ -115,11 +120,12 @@ class LoginPage:
 
     def create_table(self):
         create_table_query = """
-            CREATE TABLE IF NOT EXISTS users (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            username VARCHAR(255) NOT NULL,
-            password VARCHAR(255) NOT NULL
-            )
+            CREATE TABLE IF NOT EXISTS "users" (
+	            "id"	INTEGER UNIQUE,
+	            "username"	VARCHAR(255) NOT NULL,
+	            "password"	VARCHAR(255) NOT NULL,
+	            PRIMARY KEY("id" AUTOINCREMENT)
+            );
         """
         self._db_connector.execute_insert_query(create_table_query)
 
@@ -128,14 +134,18 @@ class LoginPage:
         password = self.password_entry.get()
 
         result = self._db_connector.execute_fetch_query(
-            "SELECT * FROM users WHERE username=? AND password=?", (username, password)
+            "SELECT ID FROM users WHERE username=? AND password=?", (username, password)
         )
 
         if result:
+            # Logged in user id is saved in global variable
+            global USER_ID
+            USER_ID = result[0][0]
+
             self.master.withdraw()
             main_app = tk.Toplevel(self.master)
             main_app.configure(background="#38726c")
-            app = YammyPizzaApp(
+            app = YummyPizzaApp(
                 main_app, database="database/shop.db"
             )  # Connecting to the next screen.
             main_app.protocol("Login Close", app.close_app)
@@ -144,7 +154,7 @@ class LoginPage:
             messagebox.showerror("Error", "Invalid login credentials")
 
 
-class YammyPizzaApp:
+class YummyPizzaApp:
     def __init__(self, master, database):
         self.master = master
         self.master.title("Yammy Pizza Information")
@@ -156,7 +166,7 @@ class YammyPizzaApp:
             highlightcolor="#38726c",
             highlightbackground="#38726c",
         )
-        self.insert_button.grid(row=5, column=0)
+        self.insert_button.grid(row=6, column=0)
 
         self.search_label = tk.Label(
             self.master, text="Enter street name (Search): ", bg="#38726c"
@@ -193,6 +203,11 @@ class YammyPizzaApp:
         self.zipcode_entry = tk.Entry(self.master)
         self.zipcode_entry.grid(row=4, column=1)
 
+        self.created_by_user = tk.Label(self.master, text="Created By", bg="#38726c")
+        self.created_by_user.grid(row=5, column=0)
+        self.created_by_user_entry = tk.Entry(self.master, state="disabled")
+        self.created_by_user_entry.grid(row=5, column=1)
+
         self.update_button = tk.Button(
             self.master,
             text="Update",
@@ -200,7 +215,7 @@ class YammyPizzaApp:
             highlightcolor="#38726c",
             highlightbackground="#38726c",
         )
-        self.update_button.grid(row=5, column=1)
+        self.update_button.grid(row=6, column=1)
 
         self.fetch_button = tk.Button(
             self.master,
@@ -209,9 +224,23 @@ class YammyPizzaApp:
             highlightcolor="#38726c",
             highlightbackground="#38726c",
         )
-        self.fetch_button.grid(row=5, column=2)
+        self.fetch_button.grid(row=6, column=2)
 
         self._connector = SQLiteConnector(database=database)
+
+    def create_table(self):
+        query = """
+            CREATE TABLE IF NOT EXISTS "address" (
+	            "street"	TEXT,
+	            "city"	TEXT,
+	            "state"	TEXT,
+	            "zip"	TEXT,
+	            "id"	INTEGER NOT NULL UNIQUE,
+	            "created_by"	INTEGER,
+	            PRIMARY KEY("id" AUTOINCREMENT)
+            );
+        """
+        self._connector.execute_insert_query(query)
 
     def insert_address(self):
         street = self.street_entry.get()
@@ -220,9 +249,11 @@ class YammyPizzaApp:
         zipcode = self.zipcode_entry.get()
 
         try:
+            # Get currently logged in user stored in the variable
+            global USER_ID
             self._connector.execute_insert_query(
-                "INSERT INTO address (STREET, CITY, STATE, ZIP) VALUES (?, ?, ?, ?)",
-                (street, city, state, zipcode),
+                "INSERT INTO address (STREET, CITY, STATE, ZIP, created_by) VALUES (?, ?, ?, ?, ?)",
+                (street, city, state, zipcode, USER_ID),
             )
             messagebox.showinfo("Success", "Address inserted successfully!")
 
@@ -233,12 +264,17 @@ class YammyPizzaApp:
         street = self.search_entry.get()
         try:
             result = self._connector.execute_fetch_query(
-                "SELECT STREET, CITY, STATE, ZIP FROM address WHERE street like ?",
+                """
+                    SELECT STREET, CITY, STATE, ZIP, u.username FROM address
+                    INNER JOIN users AS u ON u.id = CREATED_BY
+                    WHERE street like ?
+                """,
                 ("%" + street + "%",),
             )
 
             if result:
                 result = result[0]
+                self.created_by_user_entry.config(state="normal")
                 self.street_entry.delete(0, tk.END)
                 self.street_entry.insert(tk.END, result[0])
                 self.city_entry.delete(0, tk.END)
@@ -247,6 +283,9 @@ class YammyPizzaApp:
                 self.state_entry.insert(tk.END, result[2])
                 self.zipcode_entry.delete(0, tk.END)
                 self.zipcode_entry.insert(tk.END, result[3])
+                self.created_by_user_entry.delete(0, tk.END)
+                self.created_by_user_entry.insert(tk.END, result[4])
+                self.created_by_user_entry.config(state="disabled")
             else:
                 messagebox.showerror("Error", "No record found for street: " + street)
 
@@ -260,6 +299,7 @@ class YammyPizzaApp:
         zipcode = self.zipcode_entry.get()
 
         try:
+            global USER_ID
             self._connector.execute_insert_query(
                 "UPDATE address SET STREET=?, CITY=?, STATE=?, ZIP=? WHERE street=?",
                 (street, city, state, zipcode, street),
@@ -273,7 +313,13 @@ class YammyPizzaApp:
         street = self.street_entry.get()
         try:
             result = self._connector.execute_fetch_query(
-                "SELECT STREET, CITY, STATE, ZIP FROM address WHERE street=?", (street,)
+                """
+                    SELECT STREET, CITY, STATE, ZIP, u.USERNAME FROM address
+                    INNER JOIN users AS u
+                    ON u.id == created_by
+                    WHERE street=?
+                """,
+                (street,),
             )
 
             if result:
@@ -283,7 +329,7 @@ class YammyPizzaApp:
 
                 address_label = tk.Label(
                     display_window,
-                    text=f"Address: {result[0]}, {result[1]}, {result[2]} {result[3]}",
+                    text=f"Address: {result[0]}, {result[1]}, {result[2]} {result[3]}, Created By: {result[4]}",
                 )
                 address_label.pack()
 
--- sql_connector.py
+++ sql_connector.py
@@ -1,34 +1,37 @@
 import sqlite3 as sqlite
 from contextlib import contextmanager
-from typing import Dict, Optional
 
 
 class SQLiteConnector:
     def __init__(
         self,
-        database: str,
+        database,
     ):
         self._database = database
 
     @contextmanager
     def _get_connection(self):
+        # Context manager helps manage and automatically close database connection
+        # once connection goes out of context of with clause
         connection = sqlite.connect(self._database)
         try:
             yield connection
         finally:
             connection.close()
 
-    def execute_insert_query(self, query: str, parms: Optional[Dict[str, str]] = {}):
+    def execute_insert_query(self, query, parms=()):
+        # Helper function to insert or manipulate database objects
         with self._get_connection() as connection:
-            currsor = connection.cursor()
-            currsor.execute(query, parms)
-            currsor.close()
+            cursor = connection.cursor()
+            cursor.execute(query, parms)
+            cursor.close()
             connection.commit()
 
-    def execute_fetch_query(self, query: str, parms: Optional[Dict[str, str]] = {}):
+    def execute_fetch_query(self, query, parms=()):
+        # Helper functon to fetch data from database
         with self._get_connection() as connection:
-            currsor = connection.cursor()
-            currsor.execute(query, parms)
-            records = currsor.fetchall()
-            currsor.close()
+            cursor = connection.cursor()
+            cursor.execute(query, parms)
+            records = cursor.fetchall()
+            cursor.close()
             return records
